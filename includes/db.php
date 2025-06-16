418d75c07e55afe16c5f06a41bdf8cfb02ecbfe3
r.palaolozano <128861590+rpalaolozano@users.noreply.github.com>
2025-06-16 12:04:36 +0200

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,3 +1,7 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=habit_tracker', 'root', '');
+$pdo = new PDO(
+    'mysql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_NAME'),
+    getenv('DB_USER'),
+    getenv('DB_PASS')
+);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
